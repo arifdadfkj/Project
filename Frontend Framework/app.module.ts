be3557b5fce04fb5419633ddabc31cd002badfe7
arifdadfkj <arif123004@gmail.com>
2024-11-28 11:28:24 +0530

    Add files via upload

---
+++ Frontend Framework/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppAdminModule } from './app-admin/app-admin.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AppAdminModule,
+    
+  ],
+ 
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
