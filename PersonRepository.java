374c032a14537420d7252abecff4b89b8df48617
arifdadfkj <arif123004@gmail.com>
2024-11-28 11:32:12 +0530

    Delete PersonRepository.java

--- PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sign_up;
-
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-   public Person findByUsername(String username);
-}
