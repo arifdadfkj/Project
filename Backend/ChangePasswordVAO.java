c0da8478f41e188c841497aba016f70b874843bb
arifdadfkj <arif123004@gmail.com>
2024-11-28 11:30:23 +0530

    Add files via upload

---
+++ Backend/ChangePasswordVAO.java
@@ -0,0 +1,15 @@
+package com.example.sign_up;
+
+public class ChangePasswordVAO {
+	private String results;
+	
+	public void setResults(String results) {
+		this.results = results;
+	}
+
+	public String getResults() {
+		return results;
+	}
+	
+
+}
---
+++ Backend/ForgottenPasswordVAO.java
@@ -0,0 +1,14 @@
+package com.example.sign_up;
+
+public class ForgottenPasswordVAO {
+	private String results;
+
+	public String getResults() {
+		return results;
+	}
+
+	public void setResults(String results) {
+		this.results = results;
+	}
+
+}
